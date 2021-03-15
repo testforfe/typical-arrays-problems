9e879fa48c240ba8788eef11378d8bed853123ed
testforfe <79588224+testforfe@users.noreply.github.com>
2021-03-15 15:06:11 +0300

    feat: add code to task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (checkArray(array) == true) {
+    let resultMin = realSort(array);
+    return resultMin[0];
+  } else {
+      return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (checkArray(array) == true) {
+    let resultMax = realSort(array).reverse();
+    return resultMax[0];
+  } else {
+      return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (checkArray(array) == true) {
+    let resultAvg = array;
+    let average = 0;
+    for (let i = 0; i < resultAvg.length; i++) {
+        average = average + resultAvg[i];
+    }
+    average = average / resultAvg.length;
+    return average;
+  } else {
+      return 0;
+  }
+}
+
+
+
+function checkArray(array) {
+  if (typeof(array) == "object") {
+      if (array.length > 0) {
+          return true;
+      }
+  } else {
+      return false;
+  }
+}
+
+function realSort(array) {
+  let resultSort = array;
+  let temp = 0;
+  for (let i = 0; i < resultSort.length; i++) {
+      for (let j = i + 1; j < resultSort.length; j++) {
+          if(resultSort[i] > resultSort[j]) {
+              temp = resultSort[i];
+              resultSort[i] = resultSort[j];
+              resultSort[j] = temp;
+          }
+      }
+  }
+  return resultSort;
 }
